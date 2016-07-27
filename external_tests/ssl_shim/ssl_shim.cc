2539b7105028133465328afccbb93c0a92d72a69
EKR <ekr@rtfm.com>
2016-07-27 15:44:26 +0200

    Checkpoint

--- external_tests/ssl_shim/ssl_shim.cc
+++ external_tests/ssl_shim/ssl_shim.cc
@@ -3,8 +3,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-
 #include "config.h"
 
 #include <cstdlib>
@@ -12,6 +10,10 @@
 
 int main(int argc, char **argv) {
   Config cfg;
+  if (cfg.ParseArgs(argc, argv) != Config::OK) {
+    std::cerr << "Error parsing config arguments\n";
+    exit(1);
+  }
 
   auto f1 = cfg.ConfigGet("flag1", int);
   std::cerr << "flag1 " << f1 << std::endl;
